c48a913445ea00c16d8322c6b194cd87af2bddbe
ctide <christide@christide.com>
2011-09-13 15:09:43 -0700

    fix test

--- tests/app-useui-test.js
+++ tests/app-useui-test.js
@@ -20,42 +20,40 @@ var socket;
 var suite = RESTeasy.describe("UseUI");
 
 suite.next().use(lconfig.lockerHost, lconfig.lockerPort)
-	.discuss("Socket.IO")
-		.path("/socket.io/?t=test")
-		.get()
-			.expect(200)
-			.expect("returns it is ready", function(err, res, body) {
-				assert.isNull(err);
-				assert.equal(body, "Welcome to socket.io.");
-			})
-		.unpath()
-	.undiscuss()
+    .discuss("Socket.IO")
+    .path("/socket.io/?t=test")
+        .get()
+        .expect(200)
+        .expect("returns it is ready", function(err, res, body) {
+            assert.isNull(err);
+            assert.equal(body, "Welcome to socket.io.");
+        })
+    .unpath()
+    .undiscuss()
 
 // This seems to race with the locker shutting down and is being disabled for now
-/*
+//
 suite.next().suite.addBatch({
-	"Socket.IO": {
-		topic:function() {
-			socket = socketIO.connect(lconfig.lockerBase);
-			socket.on("error", function(error) {
-				console.log(error);
-				this.callback(error);
-			})
-			socket.on("counts", function(counts) {
-				console.log("Counts:",counts);
-				this.callback(false, counts);
-			});
-			setTimeout(function() {
-				console.log("Failure");
-				this.callback("timeout");
-			}, 10000);
-		},
-		"serves the counts on connect":function(err, counts) {
-			assert.isNull(err);
-			assert.isObject(counts);
-		}
-	}
+    "Socket.IO": {
+        topic:function() {
+            var promise = new (events.EventEmitter);
+            socket = socketIO.connect(lconfig.lockerBase);
+            socket.on("error", function(error) {
+                promise.emit('error', error);
+            })
+            socket.on("counts", function(counts) {
+                promise.emit('success', counts);
+            });
+            setTimeout(function() {
+                promise.emit('error', 'timeout');
+            }, 10000);
+            return promise;
+        },
+        "serves the counts on connect":function(err, counts) {
+            assert.isNull(err);
+            assert.isObject(counts);
+        }
+    }
 });
-*/
 
 suite.export(module);
