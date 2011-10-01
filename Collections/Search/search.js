a7e52af74f30c679485631b954a3a8cefacff021
Jeremie Miller <jeremie@jabber.org>
2011-09-30 19:18:16 -0500

    fix mem leak in search.js

--- Collections/Search/search.js
+++ Collections/Search/search.js
@@ -9,7 +9,7 @@
 
 var fs = require('fs'),
     locker = require('../../Common/node/locker.js');
-    
+
 var lsearch = require('../../Common/node/lsearch');
 var lutil = require('../../Common/node/lutil');
 var lconfig = require('lconfig');
@@ -86,7 +86,7 @@ exports.handleGetUpdate = function(callback) {
             console.error(error);
             return callback(err);
         }
-        
+
         reindexType(lockerInfo.lockerUrl + '/Me/contacts/allContacts', 'contact/full', 'contacts', function(err) {});
         reindexType(lockerInfo.lockerUrl + '/Me/photos/allPhotos', 'photo/full', 'photos', function(err) {});
         locker.providers('status/twitter', function(err, services) {
@@ -95,28 +95,28 @@ exports.handleGetUpdate = function(callback) {
                if (svc.provides.indexOf('status/twitter') >= 0) {
                    reindexType(lockerInfo.lockerUrl + '/Me/' + svc.id + '/getCurrent/timeline', 'timeline/twitter', 'twitter/timeline', function(err) {});
                 }
-            });     
+            });
         });
-        
+
         return callback(err);
     });
 };
 
 exports.handlePostEvents = function(req, callback) {
     var error;
-    
+
     if (req.headers['content-type'] !== 'application/json') {
         error = 'Expected content-type of "application/json" for /search/events POST request. Received content-type: ' + req.headers['content-type'];
         console.error(error);
         return callback(error, {});
     }
-        
+
     if (!req.body) {
         error = 'Empty body received for /search/events POST request.';
         console.error(error);
         return callback(error, {});
     }
-    
+
     if (req.body.hasOwnProperty('type')) {
         // FIXME Hack to handle inconsistencies between photo and contacts collection
         if (req.body.type === 'photo') {
@@ -124,12 +124,12 @@ exports.handlePostEvents = function(req, callback) {
             req.body.obj.data = req.body.obj;
         }
         // END FIXME
-        
+
         var source = getSourceForEvent(req.body);
-        
+
         if (req.body.action === 'new' || req.body.action === 'update') {
             lsearch.indexTypeAndSource(req.body.type, source, req.body.obj.data, function(err, time) {
-                if (err) { 
+                if (err) {
                     handleError(req.body.type, req.body.action, req.body.obj.data._id, err);
                     return callback(err, {});
                 }
@@ -142,8 +142,8 @@ exports.handlePostEvents = function(req, callback) {
             });
         } else if (req.body.action === 'delete') {
             lsearch.deleteDocument(req.body.obj.data._id, function(err, time) {
-                if (err) { 
-                    handleError(req.body.type, req.body.action, req.body.obj.data._id, err); 
+                if (err) {
+                    handleError(req.body.type, req.body.action, req.body.obj.data._id, err);
                     return callback(err, {});
                 }
                 handleLog(req.body.type, req.body.action, req.body.obj.data._id, time);
@@ -161,15 +161,15 @@ exports.handlePostEvents = function(req, callback) {
 
 exports.handlePostIndex = function(req, callback) {
     var error;
-    
+
     if (!req.body.type || !req.body.source || !req.body.data) {
         error = 'Invalid arguments given for /search/index POST request.';
         console.error(error);
         return callback(error, {});
     }
-    
+
     lsearch.indexTypeAndSource(req.body.type, req.body.source, req.body.data, function(err, time) {
-        if (err) { 
+        if (err) {
             handleError(req.body.type, 'new', req.body.data._id, err);
             return callback(err, {});
         }
@@ -189,7 +189,7 @@ exports.handleGetQuery = function(req, callback) {
     var q = lutil.trim(req.param('q'));
     var type;
     var limit;
-    
+
     if (req.param('type')) {
         type = req.param('type');
     }
@@ -202,8 +202,8 @@ exports.handleGetQuery = function(req, callback) {
         error = 'Please supply a valid query string for /search/query GET request.';
         console.error(error);
         return callback(error, {});
-    } 
-    
+    }
+
     function sendResults(err, results, queryTime) {
         if (err) {
             error = 'Error querying via /search/query GET request: '+JSON.stringify(err);
@@ -216,21 +216,21 @@ exports.handleGetQuery = function(req, callback) {
         enrichResultsWithFullObjects(results, function(err, richResults) {
             var data = {};
             data.took = queryTime;
-        
+
             if (err) {
                 data.error = err;
                 data.hits = [];
                 error = 'Error enriching results of /search/query GET request: ' + err;
                 return callback(error, data);
             }
-        
+
             data.error = null;
             data.hits = richResults;
             data.total = richResults.length;
             return callback(null, data);
-        });       
+        });
     }
-    
+
     if (type) {
         lsearch.queryType(type, q, {}, sendResults);
     } else {
@@ -239,28 +239,28 @@ exports.handleGetQuery = function(req, callback) {
 };
 
 exports.handleGetReindexForType = function(type, callback) {
-    // this handleGetReindex method can happen async, but deleteDocumentsByType MUST happen first before the callback.  
+    // this handleGetReindex method can happen async, but deleteDocumentsByType MUST happen first before the callback.
     // That's why we call it here
     lsearch.deleteDocumentsByType(type, function(err, indexTime) {
         callback(err, {indexTime: indexTime});
     });
-    
+
     var items;
-    
+
     if (type == 'contact/full') {
         reindexType(lockerInfo.lockerUrl + '/Me/contacts/allContacts', 'contact/full', 'contacts', function(err) {});
     }
     else if (type == 'photo/full') {
         reindexType(lockerInfo.lockerUrl + '/Me/photos/allPhotos', 'photo/full', 'photos', function(err) {});
-    }  
-    else {   
+    }
+    else {
         locker.providers(type, function(err, services) {
             if (!services) return;
             services.forEach(function(svc) {
                if (svc.provides.indexOf('timeline/twitter') >= 0) {
                     reindexType(lockerInfo.lockerUrl + '/Me/' + svc.id + '/getCurrent/home_timeline', 'timeline/twitter', 'twitter/timeline', function(err) {});
                 }
-            });     
+            });
         });
     }
 };
@@ -270,7 +270,7 @@ function reindexType(url, type, source, callback) {
         if (err) {
             console.error('Error when attempting to reindex ' + type + ' collection: ' + err);
             return callback(err);
-        } 
+        }
         if (res.statusCode >= 400) {
             var error = 'Received a ' + res.statusCode + ' when attempting to reindex ' + type + ' collection';
             console.error(err);
@@ -297,7 +297,7 @@ function reindexType(url, type, source, callback) {
                 console.error(err);
                 return callback(err);
             }
-            console.log('Reindexing of ' + type + ' completed.');           
+            console.log('Reindexing of ' + type + ' completed.');
             return callback(err);
         });
     });
@@ -312,19 +312,19 @@ function enrichResultsWithFullObjects(results, callback) {
             });
         },
         function(results, waterfallCb) {
-            async.forEachSeries(results, 
+            async.forEachSeries(results,
                 function(item, forEachCb) {
                     var url = lockerInfo.lockerUrl + '/Me/' + item._source + '/' + item._id;
                     makeEnrichedRequest(url, item, forEachCb);
-                }, 
+                },
                 function(err) {
                     waterfallCb(err, results);
                 }
-            ); 
+            );
         }
     ],
-    function(err, results) {        
-        if (err) {  
+    function(err, results) {
+        if (err) {
             return callback('Error when attempting to sort and enrich search results: ' + err, []);
         }
         return callback(null, results);
@@ -337,7 +337,7 @@ function cullAndSortResults(results, callback) {
         sortByCb(null, item._type + (1/item.score).toFixed(3));
     },
     function(err, results) {
-       callback(null, results); 
+       callback(null, results);
     });
 }
 
@@ -346,7 +346,7 @@ function makeEnrichedRequest(url, item, callback) {
         if (err) {
             console.error('Error when attempting to enrich search results: ' + err);
             return callback(err);
-        } 
+        }
         if (res.statusCode >= 400) {
             var error = 'Received a ' + res.statusCode + ' when attempting to enrich search results';
             console.error(error);
@@ -354,7 +354,7 @@ function makeEnrichedRequest(url, item, callback) {
         }
 
         item.fullobject = body;
-        
+
         if (item.fullobject.hasOwnProperty('created_at')) {
             var dateDiff = new Date(new Date().getTime() - new Date(item.fullobject.created_at).getTime());
             if (dateDiff.getUTCDate() > 2) {
@@ -376,7 +376,7 @@ function makeEnrichedRequest(url, item, callback) {
 function getSourceForEvent(body) {
     // FIXME: This is a bad hack to deal with the tech debt we have around service type naming and eventing inconsistencies
     var source;
-    
+
     if (body.type == 'contact/full' || body.type == 'photo/full') {
        var splitType = body.type.split('/');
        source = splitType[0] + 's';
@@ -426,7 +426,7 @@ process.stdin.on('data', function(data) {
 
         lsearch.setEngine(lsearch.engines.CLucene);
         lsearch.setIndexPath(process.cwd() + "/search.index");
-        
+
         app.listen(lockerInfo.port, 'localhost', function() {
             process.stdout.write(data);
         });
